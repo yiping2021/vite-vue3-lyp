82606df946eabb19c7966a40876bad520b34cc83
lyp <milk9262@qq.com>
2024-02-18 22:49:02 +0800

    ✨ feat:集成element,配置husky,环境变量，src

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SvgIcon from "@/components/SvgIcon.vue";
+</script>
 
 <template>
-  <div>app</div>
+  <div>
+    app <el-button type="primary">Primary</el-button>
+    <svg-icon name="home" color="pink" width="100px"></svg-icon>
+  </div>
 </template>
 
 <style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,14 @@
 import { createApp } from "vue";
+import ElementPlus from "element-plus";
+//@ts-ignore
+import zhCn from "element-plus/dist/locale/zh-cn.mjs";
+
+import "element-plus/dist/index.css";
+import "virtual:svg-icons-register";
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+const app = createApp(App);
+app.use(ElementPlus, {
+  locale: zhCn,
+});
+app.mount("#app");
